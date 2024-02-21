6a6aa0d1f42da87fc0d9ab0e9fd33a456b9576dc
Bernardo Hernández <contacto@rockodev.com>
2024-02-20 20:32:35 -0600

    Mejoras en el layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,16 @@
 <template>
-  <main>
+  <main class="grid min-h-[100dvh] grid-rows-[auto_1fr_auto]">
     <header class="bg-brand-red">
       <div class="flex mx-auto max-w-4xl items-center justify-start">
         <UVMLogo class="py-8 px-4 h-28 text-white" />
       </div>
     </header>
     <slot />
-    <footer class="py-2 px-4">
-      <p class="text-sm text-center">&copy; 2024</p>
+    <footer class="py-4 px-4 text-white bg-gray-700">
+      <div class="flex mb-2 items-center justify-center gap-4">
+        <a href="https://uvm.mx/aviso-de-privacidad" target="_blank" rel="noopener noreferrer" class="text-sm">Aviso de privacidad</a>
+      </div>
+      <p class="text-sm text-center">UVM Universidad del valle de México, S.C., Av Marina Nacional 180, Anáhuac 1 sección, Delegación Miguel Hidalgo, Ciudad de México, C.P. 11320.</p>
     </footer>
   </main>
 </template>
