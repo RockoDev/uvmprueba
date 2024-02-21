29c0d7230fb90b95057e68653810ecaa8c481f94
Bernardo Hernández <contacto@rockodev.com>
2024-02-20 19:10:09 -0600

    Se añade vuestic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
+  modules: ['@vuestic/nuxt'],
   postcss: {
     plugins: {
       tailwindcss: {},
