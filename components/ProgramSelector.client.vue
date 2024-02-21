fa2d054503eace3219e8192c26331223dee3a32e
Bernardo Hernández <contacto@rockodev.com>
2024-02-20 20:33:53 -0600

    Selector de programas disponibles

---
+++ components/ProgramSelector.client.vue
@@ -0,0 +1,80 @@
+<script setup>
+const props = defineProps({
+  programs: {
+    type: Array,
+    default: () => ([]),
+  },
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const selectedProgram = ref(null)
+
+const groupBy = (array, callback) => {
+  const result = {}
+  for ( const item of array ) {
+    const key = callback(item)
+    if ( !(key in result) ) result[key] = []
+    result[key].push(item)
+  }
+  return result
+}
+
+const selectors = computed(() => {
+  return Object.entries(groupBy(props.programs, ({ mkt_name }) => mkt_name)).map(([name, values]) => {
+    return {
+      name,
+      values: Object.entries(groupBy(values, ({ crmit_vertical }) => crmit_vertical)).map(([name, values]) => {
+        return {
+          name,
+          values: Object.entries(groupBy(values, ({ ofertando_crmit_name }) => ofertando_crmit_name)).map(([name, values]) => {
+            return {
+              name,
+              values,
+            }
+          }),
+        }
+      }),
+    }
+  })
+})
+
+const onSelect = selection => selectedProgram.value = selection
+
+</script>
+
+<template>
+  <div class="mx-auto max-w-sm">
+    <div v-if="isLoading">Cargando...</div>
+    <VaAccordion v-if="!isLoading">
+      <VaCollapse v-for="selector1 in selectors" :key="selector1.name" :header="selector1.name" body-color="background-element">
+        <template #body>
+          <VaAccordion>
+            <VaCollapse v-for="selector2 in selector1.values" :key="selector2.name" :header="selector2.name" body-color="#ffffff">
+              <template #header="{ value, attrs, iconAttrs, text }">
+                <div v-bind="attrs" class="py-1 px-4 hover:bg-black/20">
+                  <VaIcon
+                    name="va-arrow-down"
+                    :class="value ? '' : 'rotate-[-90deg]'"
+                    v-bind="iconAttrs"
+                  />
+                  {{ text }}
+                </div>
+              </template>
+              <template #body>
+                <div v-for="selector3 in selector2.values" :key="selector3.name" class="flex py-2 px-8 items-center justify-start gap-2 hover:bg-black/20">
+                  <span>&bull;</span>
+                  <button type="button" class="flex-1 text-left" @click.prevent="onSelect(selector3)">
+                     {{ selector3.name }}
+                  </button>
+                </div>
+              </template>
+            </VaCollapse>
+          </VaAccordion>
+        </template>
+      </VaCollapse>
+    </VaAccordion>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script setup>
+const { pending: isLoading, data } = await useFetch('/api/programs', {
+  lazy: true,
+  server: false
+})
+const programs = computed(() => data.value?.programs ?? [])
+</script>
+
 <template>
   <NuxtLayout>
     <section class="mx-auto px-4 py-8 max-w-4xl">
@@ -7,6 +15,7 @@
         <p class="text-lg mb-8">Para comenzar tu emocionante viaje hacia el conocimiento y el éxito, necesitamos saber más sobre tus intereses y metas académicas. Por favor, tómate un momento para explorar nuestras diversas carreras y selecciona aquella que realmente te apasione. Estamos aquí para apoyarte en cada paso del camino y ayudarte a alcanzar tus objetivos.</p>
         <p class="text-lg mb-8">No dudes en preguntar si necesitas más información o asistencia. ¡Esperamos verte pronto en nuestro campus y ser parte de tu viaje hacia el éxito académico!</p>
       </article>
+      <ProgramSelector :isLoading="isLoading" :programs="programs" />
     </section>
   </NuxtLayout>
 </template>
